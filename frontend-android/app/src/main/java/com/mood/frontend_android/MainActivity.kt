1bf096c13f717c131d04688ac74c2f2e3018ce53
Vladdjuga <vladdjuga@gmail.com>
2025-10-15 17:56:51 +0300

    Add MainActivity class and update deployment target selector and build.gradle.kts

---
+++ frontend-android/app/src/main/java/com/mood/frontend_android/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mood.frontend_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            //UI components here
+        }
+    }
+}
