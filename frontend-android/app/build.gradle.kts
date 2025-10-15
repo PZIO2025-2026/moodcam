c3bca7c37b116ca55b43caab0ce14df11e82c88b
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-10-15 14:01:55 +0200

    Add the Firebase dependency.
    Add the plugin as a dependency to Root-level Gradle file.
    Add the google-services plugin.

--- frontend-android/app/build.gradle.kts
+++ frontend-android/app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -61,4 +65,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+
+    implementation("com.google.firebase:firebase-analytics")
+
 }
