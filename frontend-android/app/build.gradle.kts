dc7884d5090078e8ffa96a887600536deb6709a4
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-10-15 14:03:46 +0200

    Add the dependencies for Firebase Authentication and Cloud Firestore.

--- frontend-android/app/build.gradle.kts
+++ frontend-android/app/build.gradle.kts
@@ -69,6 +69,10 @@ dependencies {
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
 
+    // Add the dependency for the Firebase SDK for Google Analytics
     implementation("com.google.firebase:firebase-analytics")
 
+    // Add the dependencies for Firebase Authentication and Cloud Firestore
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
