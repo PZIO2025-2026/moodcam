c30f8fb250cf390bb2dc529a2b6aee8c781d14b1
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-10-19 14:39:45 +0200

    Created Reusable Authentication UI Components
    Develop reusable UI components:
    EmailAuth Field
    PasswordAuth Field
    AuthScreenLayout

---
+++ frontend-android/app/src/main/java/com/moodcam/frontend_android/auth/ui/elements/AuthScreenLayout.kt
@@ -0,0 +1,31 @@
+package com.moodcam.frontend_android.auth.ui.elements
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AuthScreenLayout(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "MOODCAM",
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
+        content()
+    }
+}
